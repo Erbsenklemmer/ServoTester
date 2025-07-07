065e7aa23a0071572b8eb4d6778355b5873f3eb8
Jan <jan@Laptop-T7>
2025-07-07 23:00:28 +0200

    Ohne externes EncoderTool

--- ServoTester.ino
+++ ServoTester.ino
@@ -1,7 +1,6 @@
 #include <U8g2lib.h>
 #include <Wire.h>
 
-#include <EncoderTool.h>
 #include <Bounce2.h>
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, U8X8_PIN_NONE, PIN_A5, PIN_A4);
@@ -11,7 +10,14 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, U8X8_PIN_NONE, PIN_A5, PIN_
 //Adafruit_SSD1306 display(1);
 
 int count(0);
-EncoderTool::PolledEncoder encoder;
+volatile byte EncoderStatus = 200;
+
+int previousMilis = 0;
+int displayUpdateMilis = 250;
+
+int currentPos = 0;
+int pressedState = 0;
+
 Bounce2::Button button;
 
 #define BUTTON_HOLD 1000
@@ -19,73 +25,101 @@ Bounce2::Button button;
 void setup() {
   Serial.begin(9600);
 
-  //encoder.begin(2, 3);
-  encoder.begin(PIN2, PIN3);//, PIN4);
+  pinMode(PIN3, INPUT_PULLUP);
+  pinMode(PIN4, INPUT_PULLUP);
 
-  button.attach(PIN4, INPUT_PULLUP);
+  button.attach(PIN5, INPUT_PULLUP);
   button.interval(5);
-
-  
 
   display.begin();
   display.setFontDirection(0);
   display.setFontRefHeightExtendedText();
 
-  display.setFont(u8g2_font_6x10_tf);
   display.setDrawColor(1);
   display.setFontPosTop();
 
-  display.clearBuffer();
-  display.drawStr(0, 0, "drawBox");
+  display.clearDisplay();
+  display.display();
 
-  display.drawBox(5, 10, 20, 10);
-
-  display.drawStr(60, 0, "drawFrame");
-  display.drawFrame(65, 10, 20, 10);
-
-  display.sendBuffer();
+  attachInterrupt(digitalPinToInterrupt(PIN3), Encoder, RISING);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  encoder.tick();
+
   button.update();
 
 
-  //display draw something
-  //display.updateDisplayArea(x, y, w, h);
-
-  if (encoder.valueChanged()) {
-    Serial.println(encoder.getValue());
-  }
-
-  int pressedState = 0;
-  if (button.changed())
-  {
-    if (button.isPressed() != LOW)
-    {
+  pressedState = 0;
+  if (button.changed()) {
+    if (button.isPressed() != LOW) {
       if (button.previousDuration() < BUTTON_HOLD) {
         Serial.println("Short klick");
         pressedState = 1;
-      }
-      else {
+      } else {
         Serial.println("Long klick");
         pressedState = 2;
       }
-    }
-    else
+    } else
       Serial.println("Start pressing");
     Serial.print("Pressed: ");
     Serial.println(pressedState);
   }
 
-  // display.clearBuffer();
-  // display.drawStr(60, 0, "drawFrame");
-  // display.drawFrame(65, 10, 20, 10);
+  //if (millis() - previousMilis > displayUpdateMilis)
+  {
+    //Serial.println("UpdateDisplay");
+    display.clearBuffer();
 
-  // display.sendBuffer();
-  // Serial.print("Hallo3 ");
-  // Serial.println(count);
-  // count++;
-   //delay(0);
+    display.setFont(u8g2_font_6x10_tf);
+
+    display.drawStr(0, 0, "drawBox");
+    display.drawBox(5, 10, 20, 10);
+
+    display.drawStr(60, 0, "drawFrame");
+    display.drawFrame(65, 10, 20, 10);
+
+  int i = currentPos;
+    String posOut = String(i, 10);
+    //display.drawStr(20, 40, posOut.c_str());
+
+    Serial.println(posOut);
+
+    display.setFont(u8g2_font_helvB14_te);
+
+    display.drawStr(20, 40, posOut.c_str());
+
+    // String pressedStr;
+    // if (pressedState == 1)
+    //   pressedStr = "short";
+    // else if (pressedState == 2)
+    //   pressedStr = "long";
+
+    //display.drawStr(60, 40, pressedStr.c_str());
+    
+    // //display.drawUTF8(20, 40, h.c_str());
+    // Serial.println(pressedStr);
+
+    display.sendBuffer();
+
+    // if (millis() - previousMilis > displayUpdateMilis) {
+    //   previousMilis = millis();
+    //   pressedState = "";
+    // }
+
+    if (EncoderStatus != 200) {
+      if (EncoderStatus & B00010000)
+        currentPos--;
+      else
+      {
+        currentPos++;
+      }
+
+      EncoderStatus = 200;
+    }
+  }
+}
+
+void Encoder() {
+  EncoderStatus = PIND;
 }
